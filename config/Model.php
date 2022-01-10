0e78eaf4f45251c038804d8034ce4de82e1adbaf
Xavier <78607845+JbSense@users.noreply.github.com>
2022-01-10 09:40:38 -0300

    Update Model.php

--- config/Model.php
+++ config/Model.php
@@ -2,17 +2,9 @@
 
 class Model {
     public function __construct() {
-        /**
-         * Seta o nome da tabela com o nome da Model que a instanciou.
-         */
         $this->table = strtolower(get_class($this))."s";
     }
 
-    /**
-     * Cria a conexão com o banco.
-     * 
-     * @return function -> conexão.
-     */
     private function connect() {
         $HOST = 'localhost';
         $DATABASE = 'template';
@@ -22,11 +14,6 @@ class Model {
         return new PDO("mysql:host=$HOST;dbname=$DATABASE", $USER, $PASSWORD);
     }
 
-    /**
-     * Executa um insert no banco.
-     * 
-     * @param {array}
-     */
     public function create($data) {
         if(array_keys($data) != $this->rows) return false; // Valida se os campos passados são autorizados.
 
@@ -41,11 +28,6 @@ class Model {
         return $sql;
     }
 
-    /**
-     * Realiza um delete no banco.
-     * 
-     * @param {int} $id
-     */
     public function delete($id) {
         $conn = $this->connect();
         $sql = $conn->prepare("DELETE FROM $this->table WHERE id = $id");
@@ -54,13 +36,6 @@ class Model {
         return $sql;
     }
 
-    /**
-     * Executa um select no banco.
-     * 
-     * @param {string} $id.
-     * 
-     * @return {array} -> dados da resultantes da consulta.
-     */
     public function get($cond = null) {
         $conn = $this->connect();
 
